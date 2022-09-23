1479750fbe8cbd6ba02107aac5d3efb8a125ba9f
zyy <zyy2008@vip.qq.com>
2022-09-23 14:53:48 +0800

    新增路由样式

--- .umirc.ts
+++ .umirc.ts
@@ -2,8 +2,23 @@ import { defineConfig } from "@umijs/max";
 
 export default defineConfig({
   layout: {
-    title: "123",
+    title: "121113",
   },
-  antd: {},
+  routes: [
+    {
+      path: "/",
+      redirect: "/home",
+    },
+    {
+      name: "首页",
+      path: "/home",
+      component: "./index",
+    },
+    {
+      name: "权限演示",
+      path: "/access",
+      component: "./index",
+    },
+  ],
   npmClient: "yarn",
 });
