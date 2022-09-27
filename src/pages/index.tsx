aac6ac2debd3546b9b6753f8ecf2a2e3f645ece8
zyy <zyy2008@vip.qq.com>
2022-09-27 13:52:22 +0800

    新增drag拖拽

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,38 @@
-import yayJpg from '../assets/yay.jpg';
+import {
+  XFlow,
+  FlowchartCanvas,
+  CanvasToolbar,
+  FlowchartNodePanel,
+  FlowchartExtension,
+} from "@antv/xflow";
+import { useToolbarConfig } from "./toolbar-config";
+import "antd/dist/antd.css";
+import "@antv/xflow/dist/index.css";
+import "./index.less";
 
-export default function HomePage() {
-  return (
-    <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
-    </div>
-  );
+/**  Demo Props  */
+export interface IDemoProps {
+  anything: string;
 }
+
+const XFlowDemo: React.FC<{}> = (props) => {
+  const toolbarConfig = useToolbarConfig(props);
+  return (
+    <XFlow className="xflow-workspace">
+      <FlowchartExtension />
+      <FlowchartNodePanel
+        position={{ width: 162, top: 40, bottom: 0, left: 0 }}
+      />
+      <FlowchartCanvas position={{ top: 0, bottom: 0, left: 0, right: 0 }}>
+        <CanvasToolbar
+          layout="horizontal"
+          config={toolbarConfig}
+          position={{ top: 0, left: 0, right: 0, height: 40 }}
+        />
+        {/* <CanvasMiniMap position={{ bottom: 12, left: 12 }} /> */}
+      </FlowchartCanvas>
+    </XFlow>
+  );
+};
+
+export default XFlowDemo;
