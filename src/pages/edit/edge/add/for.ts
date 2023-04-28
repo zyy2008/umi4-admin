47dc96083952c47332ab804050246aca6938a708
zyy <zyy2008@vip.qq.com>
2023-04-28 11:06:01 +0800

    tijiao

--- src/pages/edit/edge/add/for.ts
+++ src/pages/edit/edge/add/for.ts
@@ -8,44 +8,44 @@ export default async function handle(args: Args, isFor = true) {
   const { id: targetId } = targetCell;
   const { sourcePortId } = edgeCell?.getData();
   const port = sourceCell.getPort(sourcePortId);
-  if (port?.group === "bottom") {
-    // const { group } = targetCell.getData();
-    // edgeCell?.setLabels("true");
-    // edgeCell?.setData({
-    //   ...edgeCell.getData(),
-    //   label: "true",
-    // });
-    // const groupId = uuidv4();
-    // const res = await commandService.executeCommand<
-    //   NsGroupCmd.AddGroup.IArgs,
-    //   NsGroupCmd.AddGroup.IResult
-    // >(XFlowGroupCommands.ADD_GROUP.id, {
-    //   nodeConfig: {
-    //     id: groupId,
-    //     isCollapsed: false,
-    //     groupChildren: [targetId],
-    //     renderKey: "GroupNode",
-    //     groupPadding: 40,
-    //     groupHeaderHeight: 0,
-    //     parent: true,
-    //     init: true,
-    //     label: isFor ? "" : (port.tooltip as string).split(":")[1],
-    //   },
-    // });
-    // console.log(res?.contextProvider().getResult());
-    // setTimeout(() => {
-    //   const { nodeCell } = res?.contextProvider().getResult();
-    //   console.log(nodeCell);
-    //   const node = x6Graph.getCellById(group);
-    //   node.addChild(nodeCell);
-    // }, 3000);
-    // isFor &&
-    //   addEdge({
-    //     x6Graph,
-    //     sourceId,
-    //     targetId: groupId,
-    //   });
-  }
+  // if (port?.group === "bottom") {
+  // const { group } = targetCell.getData();
+  // edgeCell?.setLabels("true");
+  // edgeCell?.setData({
+  //   ...edgeCell.getData(),
+  //   label: "true",
+  // });
+  // const groupId = uuidv4();
+  // const res = await commandService.executeCommand<
+  //   NsGroupCmd.AddGroup.IArgs,
+  //   NsGroupCmd.AddGroup.IResult
+  // >(XFlowGroupCommands.ADD_GROUP.id, {
+  //   nodeConfig: {
+  //     id: groupId,
+  //     isCollapsed: false,
+  //     groupChildren: [targetId],
+  //     renderKey: "GroupNode",
+  //     groupPadding: 40,
+  //     groupHeaderHeight: 0,
+  //     parent: true,
+  //     init: true,
+  //     label: isFor ? "" : (port.tooltip as string).split(":")[1],
+  //   },
+  // });
+  // console.log(res?.contextProvider().getResult());
+  // setTimeout(() => {
+  //   const { nodeCell } = res?.contextProvider().getResult();
+  //   console.log(nodeCell);
+  //   const node = x6Graph.getCellById(group);
+  //   node.addChild(nodeCell);
+  // }, 3000);
+  // isFor &&
+  //   addEdge({
+  //     x6Graph,
+  //     sourceId,
+  //     targetId: groupId,
+  //   });
+  // }
   if (port?.group === "left") {
     edgeCell?.setLabels("false");
     edgeCell?.setData({
